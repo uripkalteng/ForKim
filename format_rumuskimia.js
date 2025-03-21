cfafd73e7a58b7d46d337150a558a3a1848e7119
uripkalteng <uripkalteng@users.noreply.github.com>
2025-03-22 03:36:02 +0700

    Update format_rumuskimia.js

--- format_rumuskimia.js
+++ format_rumuskimia.js
@@ -8,11 +8,27 @@ document.addEventListener('DOMContentLoaded', function() {
                 var formattedText = text.replace(/\\(.*?)\\/g, function(match, formula) {
                     var result = '';
                     for (var i = 0; i < formula.length; i++) {
-                        if (/\d/.test(formula[i]) && i > 0 && formula[i-1] !== '.') {
-                            result += '<sub>' + formula[i] + '</sub>';
-                        } else if (formula[i] === '<' && i + 2 < formula.length && formula[i + 1] === '=' && formula[i + 2] === '>') {
+                        if (formula[i] === '<' && i + 2 < formula.length && formula[i + 1] === '=' && formula[i + 2] === '>') {
                             result += '\u21CC'; // Panah ⇌
                             i += 2;
+                        } else if (formula[i] === '^' && i + 1 < formula.length && /[+-]/.test(formula[i + 1])) {
+                            // Tangani superscript untuk muatan (misalnya ^2-)
+                            var chargeChar = formula[i + 1]; // + atau -
+                            var chargeNumber = '';
+                            var j = i + 2;
+                            while (j < formula.length && /\d/.test(formula[j])) {
+                                chargeNumber += formula[j];
+                                j++;
+                            }
+                            if (chargeNumber) {
+                                result += '<sup>' + chargeNumber + chargeChar + '</sup>'; // Urutan: 2-
+                            } else {
+                                result += '<sup>' + chargeChar + '</sup>'; // Hanya + atau -
+                            }
+                            i = j - 1;
+                        } else if (/\d/.test(formula[i]) && i > 0 && formula[i-1] !== '.' && formula[i-1] !== '^') {
+                            // Tangani subskrip hanya untuk angka setelah huruf, bukan setelah ^
+                            result += '<sub>' + formula[i] + '</sub>';
                         } else if (formula[i] === '(' && i + 1 < formula.length) {
                             var j = i + 1;
                             var state = '';
@@ -21,32 +37,15 @@ document.addEventListener('DOMContentLoaded', function() {
                                 j++;
                             }
                             if (j < formula.length && formula[j] === ')') {
-                                // Hanya wujud zat yang italik
                                 if (state === 's' || state === 'l' || state === 'g' || state === 'aq') {
                                     result += '<i>(' + state + ')</i>';
-                                    console.log('Wujud zat ditemukan: ' + state + ', diformat italik');
                                 } else {
                                     result += '(' + state + ')';
-                                    console.log('Kurung biasa ditemukan: ' + state + ', tidak italik');
                                 }
                                 i = j;
                             } else {
                                 result += formula[i];
                             }
-                        } else if (formula[i] === '^' && i + 1 < formula.length && /[+-]/.test(formula[i + 1])) {
-                            var chargeChar = formula[i + 1];
-                            var chargeNumber = '';
-                            var j = i + 2;
-                            while (j < formula.length && /\d/.test(formula[j])) {
-                                chargeNumber += formula[j];
-                                j++;
-                            }
-                            if (chargeNumber) {
-                                result += '<sup>' + chargeChar + chargeNumber + '</sup>';
-                            } else {
-                                result += '<sup>' + chargeChar + '</sup>';
-                            }
-                            i = j - 1;
                         } else {
                             result += formula[i];
                         }
