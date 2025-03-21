3afc5b77dff1ecea904d0acd0c4a6fb919e83f49
uripkalteng <uripkalteng@users.noreply.github.com>
2025-03-22 03:45:38 +0700

    Perbaikan format muatan ion format_rumuskimia.js

--- format_rumuskimia.js
+++ format_rumuskimia.js
@@ -11,23 +11,42 @@ document.addEventListener('DOMContentLoaded', function() {
                         if (formula[i] === '<' && i + 2 < formula.length && formula[i + 1] === '=' && formula[i + 2] === '>') {
                             result += '\u21CC'; // Panah ⇌
                             i += 2;
-                        } else if (formula[i] === '^' && i + 1 < formula.length && /[+-]/.test(formula[i + 1])) {
-                            // Tangani superscript untuk muatan (misalnya ^2-)
-                            var chargeChar = formula[i + 1]; // + atau -
-                            var chargeNumber = '';
-                            var j = i + 2;
-                            while (j < formula.length && /\d/.test(formula[j])) {
-                                chargeNumber += formula[j];
+                        } else if (formula[i] === '^' && i + 1 < formula.length) {
+                            // Tangani superscript untuk muatan (misalnya ^(2-) atau ^2-)
+                            var charge = '';
+                            var j = i + 1;
+                            if (formula[j] === '(') { // Format ^(2-)
                                 j++;
+                                while (j < formula.length && formula[j] !== ')') {
+                                    charge += formula[j];
+                                    j++;
+                                }
+                                if (j < formula.length && formula[j] === ')') {
+                                    j++;
+                                }
+                            } else { // Format ^2-
+                                while (j < formula.length && /[0-9+-]/.test(formula[j])) {
+                                    charge += formula[j];
+                                    j++;
+                                }
                             }
-                            if (chargeNumber) {
-                                result += '<sup>' + chargeNumber + chargeChar + '</sup>'; // Urutan: 2-
+                            if (charge) {
+                                // Pisahkan angka dan tanda muatan
+                                var chargeNumber = charge.match(/\d+/) ? charge.match(/\d+/)[0] : '';
+                                var chargeSign = charge.match(/[+-]/) ? charge.match(/[+-]/)[0] : '';
+                                if (chargeNumber && chargeSign) {
+                                    result += '<sup>' + chargeNumber + chargeSign + '</sup>';
+                                } else if (chargeSign) {
+                                    result += '<sup>' + chargeSign + '</sup>';
+                                } else if (chargeNumber) {
+                                    result += '<sup>' + chargeNumber + '</sup>';
+                                }
+                                i = j - 1;
                             } else {
-                                result += '<sup>' + chargeChar + '</sup>'; // Hanya + atau -
+                                result += formula[i];
                             }
-                            i = j - 1;
-                        } else if (/\d/.test(formula[i]) && i > 0 && formula[i-1] !== '.' && formula[i-1] !== '^') {
-                            // Tangani subskrip hanya untuk angka setelah huruf, bukan setelah ^
+                        } else if (/\d/.test(formula[i]) && i > 0 && /[A-Za-z]/.test(formula[i-1])) {
+                            // Subskrip hanya untuk angka setelah huruf
                             result += '<sub>' + formula[i] + '</sub>';
                         } else if (formula[i] === '(' && i + 1 < formula.length) {
                             var j = i + 1;
